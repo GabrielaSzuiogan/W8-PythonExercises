de2f25ba6353d89cd1f4ccd84f16f0d02a2d9828
GabrielaSzuiogan <Gabriela.Szuiogan@cognizant.com>
2026-03-05 14:45:49 +0200

    restructured

--- d4/d4-p.py
+++ d4/d4-p.py
@@ -1,10 +1,11 @@
 import file_functions
 
+#p1
 file_content = file_functions.read_file("./d4/source.txt")
 file_destination = "./d4/copy.txt"
 
 file_functions.write_content(file_destination, file_content)
+# file_functions.delete_content(file_destination)
 
-file_functions.delete_content(file_destination)
-
+#p2
 file_functions.read_file_lines("./d4/story.txt")
